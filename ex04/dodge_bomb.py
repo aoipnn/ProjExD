289ec0a9a4112bce406f811d971633cf8d6631bb
aoi <c0a21152cd@edu.teu.ac.jp>
2022-07-05 14:36:10 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,7 +1,9 @@
+from re import T
 import pygame as pg
 import sys
 
 def main():
+    #練習1
     pg.display.set_caption("逃げろ! こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))           #Surface
     # screen_rct = screen_sfc.getrect()                     #Rect
@@ -13,19 +15,37 @@ def main():
     # pg.display.update()
 
     kkimg_sfc = pg.image.load("ex04/fig/6.png")    #Surface
-    pg.transform.rotozoom(kkimg_sfc, 0, 5.0)      #Surface
+    pg.transform.rotozoom(kkimg_sfc, 0, 2.0)      #Surface
     kkimg_rct = kkimg_sfc.get_rect()                #Rect
     kkimg_rct.center = 900, 400
 
     while True:
-        screen_sfc.blit(bgimg_sfc, bgimg_rct)    
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        screen_sfc.blit(bgimg_sfc, bgimg_rct) 
 
         #練習2
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        #練習4
+        key_states = pg.key.get_pressed()
+        if key_states[pg.K_UP] == True: 
+            kkimg_rct.centery -= 1     #Y座標を-1
+        if key_states[pg.K_DOWN] == True:
+            kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT] == True:
+            kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT] == True:
+            kkimg_rct.centerx += 1
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
+  
+            
+
         pg.display.update()
         clock.tick(1000)   
+
+
+    
                  
 
 
