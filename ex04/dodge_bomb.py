2295b8b9efe64b42a9949543654ee662a89bbd42
aoi <c0a21152cd@edu.teu.ac.jp>
2022-07-05 14:24:23 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,16 +4,22 @@ import sys
 def main():
     pg.display.set_caption("逃げろ! こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))           #Surface
-    # screen_rct = screen_sfc.getrect()                       #Rect
-    bgimg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")                     #Surface
+    # screen_rct = screen_sfc.getrect()                     #Rect
+    bgimg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")         #Surface
     bgimg_rct = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rct)   
     clock = pg.time.Clock()                    #時間計測用のオブジェクト
     # clock.tick(0.2)                              #1fpsの時を刻む
     # pg.display.update()
 
+    kkimg_sfc = pg.image.load("ex04/fig/6.png")    #Surface
+    pg.transform.rotozoom(kkimg_sfc, 0, 5.0)      #Surface
+    kkimg_rct = kkimg_sfc.get_rect()                #Rect
+    kkimg_rct.center = 900, 400
+
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)    
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         #練習2
         for event in pg.event.get():
